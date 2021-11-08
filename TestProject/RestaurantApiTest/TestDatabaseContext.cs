4b3837b948dec3d56d2ace124773c84872de6e0f
Yash Kushwaha <yash.kushwaha@globallogic.com>
2021-11-08 13:22:55 +0530

    ci

--- TestProject/RestaurantApiTest/TestDatabaseContext.cs
+++ TestProject/RestaurantApiTest/TestDatabaseContext.cs
@@ -1,6 +1,16 @@
-﻿namespace TestProject.RestaurantApiTest
+﻿using MongoDB.Driver;
+
+namespace TestProject.RestaurantApiTest
 {
     class TestDatabaseContext
     {
+        readonly MongoClient client;
+        readonly IMongoDatabase db;
+        public TestDatabaseContext()
+        {
+            client = new MongoClient("mongodb://localhost:27017/");
+            db = client.GetDatabase("TestRestaurantDB");
+        }
+        public IMongoCollection<RestaurantAPI.Models.Restaurant> Restaurants => db.GetCollection<RestaurantAPI.Models.Restaurant>("Restaurants");
     }
 }
